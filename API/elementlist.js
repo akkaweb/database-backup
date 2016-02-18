5ef7897b1f89ed89a77ce2046aa78a370d0a4876
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-02-18 16:40:23 +0100

    added APIs

---
+++ API/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","DatabaseBackup\\Shell\\BackupShell"],["c","DatabaseBackup\\Utility\\BackupManager"],["c","DatabaseBackup\\Utility\\DatabaseExport"]];
