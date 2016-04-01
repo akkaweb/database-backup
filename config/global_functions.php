c92172c6685a31869574490cdaf4d7ee344a1cd8
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-04-01 23:55:21 +0200

    added `which()` global function. Moved functions

---
+++ config/global_functions.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of DatabaseBackup.
+ *
+ * DatabaseBackup is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * DatabaseBackup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DatabaseBackup.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ */
+
+if(!function_exists('get_compression')) {
+	/**
+	 * Gets the compression type from the file extension
+	 * @param string $extension File extension
+	 * @return string Compression type
+	 * @throws InternalErrorException
+	 */
+	function get_compression($extension) {
+        if(!array_key_exists($extension, $GLOBALS['supported_extensions']))
+			throw new InternalErrorException(__d('database_backup', 'The {0} extension is not supported', $extension));
+        
+        return $GLOBALS['supported_extensions'][$extension];
+	}
+}
+
+if(!function_exists('get_extension')) {
+    /**
+	 * Gets the file extension from the compression type
+     * @param string $compression Compression type
+     * @return string File extension
+     * @throws InternalErrorException
+     */
+    function get_extension($compression) {
+        $value = array_search($compression, $GLOBALS['supported_extensions']);
+        
+        if(!$value)
+			throw new InternalErrorException(__d('database_backup', 'The {0} compression is not supported', $compression));
+        
+        return $value;
+    }
+}
+
+if(!function_exists('which')) {
+	/**
+	 * Executes the `which` command.
+	 * 
+	 * It shows the full path of (shell) commands.
+	 * @param string $command Command
+	 * @return string Full path of command
+	 */
+	function which($command) {
+		return exec(sprintf('which %s', $command));
+	}
+}
